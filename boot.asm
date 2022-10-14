8480e7f1ee65f643711265ae5092c5189a38585a
Paweł Cichowski <cichowski.pawel1@gmail.com>
2022-10-14 16:20:29 +0200

    boot.asm: print hello world to console

--- boot.asm
+++ boot.asm
@@ -2,11 +2,28 @@ ORG 0x7c00
 BITS 16
 
 start:
-    mov ah, 0eh
-    mov al, 'A'
-    int 0x10        ; call BIOS routine to output A to the console
+    mov si, message ; load message label address
+    call print
 
     jmp $           ; do not go to lower instructions
+
+print:
+.loop:
+    lodsb ; load char pointed by si to al
+    cmp al, 0
+    je .done ; done printing if encountered '0' char
+    call print_char
+    jmp .loop
+
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'hello world', 0
 
 times 510- ($ - $$) db 0 ; zero out 510 bytes of data
 dw 0xAA55                ; add boot signature

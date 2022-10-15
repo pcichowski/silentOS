9d89cd1889743458763a7cc8fbcd16d9c34db2be
Paweł Cichowski <cichowski.pawel1@gmail.com>
2022-10-15 11:51:39 +0200

    boot.asm: enter protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0x0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
     jmp short start ; initialize BPB according to the specification
@@ -8,56 +11,66 @@ _start:
 times 33  db 0 ; zero out the rest of the BPB
 
 start:
-    jmp 0x7c0:step2 ; set code segment to 0x7c0
+    jmp 0:step2 ; set code segment to 0x7c0
 
 step2:
     cli     ; clear (diable) interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax ; set data segments to be universal for all bioses
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
     sti     ; enable interrupts
 
-    mov ah, 2 ; read sector command
-    mov al, 1 ; read one sector
-    mov ch, 0 ; low eight bits
-    mov cl, 2
-    mov dh, 0
-    mov bx, buffer
-    int 0x13
-    jc error
 
-    mov si, buffer
-    call print
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    jmp $           ; do not go to lower instructions
 
-error:
-    mov si, error_message
-    call print
+; -- GDT --
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+; offset 0x8
+gdt_code:
+    dw 0xffff   ; first 0-15 bits
+    dw 0        ; base 0-15 bits
+    db 0        ; base 16-23 bits
+    db 0x9a     ; access byte
+    db 11001111b    ; high and low 4 bit flahs
+    db 0 ; base 24-31 bits
+; offset 0x10
+gdt_data: ; DS, SS, ES, FS, GS
+    dw 0xffff   ; first 0-15 bits
+    dw 0        ; base 0-15 bits
+    db 0        ; base 16-23 bits
+    db 0x92     ; access byte of data segment
+    db 11001111b    ; high and low 4 bit flahs
+    db 0 ; base 24-31 bits
+gdt_end:
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax ; set data segments to their GDT
+
+    mov ebp, 0x00200000
+    mov esp, ebp
+
     jmp $
-
-print:
-.loop:
-    lodsb ; load char pointed by si to al
-    cmp al, 0
-    je .done ; done printing if encountered '0' char
-    call print_char
-    jmp .loop
-
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db 'Failed to load sector', 0
 
 times 510- ($ - $$) db 0 ; zero out 510 bytes of data
 dw 0xAA55                ; add boot signature
-
-buffer:

788d26ec78bb78f638a2fadc04b38cda20816ad9
Paweł Cichowski <cichowski.pawel1@gmail.com>
2022-10-15 16:56:40 +0200

    kernel.c: printing to console and hello world

--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,76 @@
 #include "kernel.h"
 
-void kernel_main()
+uint16_t *video_mem = 0;
+
+uint16_t console_row = 0;
+uint16_t console_col = 0;
+
+/*
+ * Clears the console screen
+ */
+void
+console_initialize()
 {
-    char *video_mem = (char*)(0xB8000);
+    video_mem = (uint16_t*)(0xB8000);
 
-    video_mem[0] = 'A';
-    video_mem[1] = 3;
+    for (int i = 0; i < VGA_HEIGHT; i++) {
+        for (int j = 0; j < VGA_WIDTH; j++) {
+            console_putchar(j, i, ' ', 0);
+        }
+    }
+}
 
-    video_mem[2] = 'C';
-    video_mem[3] = 2;
+size_t
+strlen(const char *str)
+{
+    size_t len = 0;
+    while(str[len]) {
+        len += 1;
+    }
+    return len;
+}
+
+void
+console_putchar(int x, int y, char a, char colour)
+{
+    video_mem[(y * VGA_WIDTH) + x] = console_char(a, colour);
+}
+
+void
+console_writechar(char c, char colour)
+{
+    console_putchar(console_col, console_row, c, colour);
+
+    console_col += 1;
+
+    if (console_col >= VGA_WIDTH) {
+        console_col = 0;
+        console_row += 1;
+    }
+}
+
+/*
+ * Makes a correct console character with a specified background
+ */
+uint16_t
+console_char(char a, char colour)
+{
+    return (colour << 8 ) | a;
+}
+
+void
+print(const char *str)
+{
+    size_t len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        console_writechar(str[i], 15);
+    }
+}
+
+void
+kernel_main()
+{
+    console_initialize();
+
+    print("hello world, the print function is working correctly");
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,6 +1,17 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#include <stdint.h>
+#include <stddef.h>
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 24
+
+
 void kernel_main();
+
+uint16_t console_char(char a, char color);
+void console_initialize();
+void console_putchar(int x, int y, char a, char colour);
 
 #endif

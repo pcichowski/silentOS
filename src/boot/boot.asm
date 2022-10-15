71a31ffd07877616883a89aa5d85134e20784ceb
Paweł Cichowski <cichowski.pawel1@gmail.com>
2022-10-15 12:13:11 +0200

    Enable the A20 address bus line

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -58,17 +58,31 @@ gdt_descriptor:
     dw gdt_end - gdt_start-1
     dd gdt_start
 
+;
+; ---- protected mode starts here ----
+;
 [BITS 32]
 load32:
+    ; set data segments to the correct GDT
     mov ax, DATA_SEG
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
-    mov ss, ax ; set data segments to their GDT
+    mov ss, ax
 
+    ; move the stack furthet
     mov ebp, 0x00200000
     mov esp, ebp
+
+    ; enable the A20 line
+    in al, 0x92
+    test al, 2
+    jnz .after
+    or al, 2
+    and al, 0xFE
+    out 0x92, al
+.after:
 
     jmp $
 
